e6986cd6de1efa1855d84796f463fb03d3371d8b
ytodorov <ytodorov@ytodorov.com>
2020-04-26 16:29:13 +0300

    Initial

---
+++ Free-Email-Checker/Free-Email-Checker/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Free_Email_Checker.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
